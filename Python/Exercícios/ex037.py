296f7444bcff9a847e34e98a2f0f3fcdb4a54cd7
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:50:08 -0300

    Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.

--- Python/Exercícios/ex037.py
+++ Python/Exercícios/ex037.py
@@ -1,18 +1,19 @@
 """
 Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de
 conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
-"""
-"""
+
 bin() inteiro para binário
 oct() inteiro para octal
 hex() inteiro para hexadecimal
 """
+
 num = int(input('Digite um número inteiro: '))
 print('''Escolha uma das bases para conversão:
 [ 1 ] converter para BINÁRIO
 [ 2 ] converter para OCTAL
 [ 3 ] converter para HEXADECIMAL''')
 opt = int(input('Sua opção: '))
+
 while opt != 1 and opt != 2 and opt != 3:
     opt = int(input('Sua opção: '))
 
