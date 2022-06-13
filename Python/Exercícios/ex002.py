f4124ffb473f8b8faa2ff4a73a10dbc45d635ad2
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 02:00:27 -0300

    Crie um programa que leia o dia, mês e ano de nascimento de uma pessoa e mostre a data formatada.

--- Python/Exercícios/ex002.py
+++ Python/Exercícios/ex002.py
@@ -1,4 +1,8 @@
+"""
+Exercício Python 2: Crie um programa que leia o dia, mês e ano de nascimento de uma pessoa e mostre a data formatada.
+"""
 dia = input('Dia = ')
 mes = input('Mes = ')
 ano = input('Ano = ')
-print('Você nasceu no dia',dia, 'de', mes, 'de', ano, '. Correto?')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
+# print(f'Você nasceu no dia {dia} de {mes} de {ano}. Correto?')
