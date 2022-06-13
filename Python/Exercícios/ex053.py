970e3ec2c99fb5ec442ea292d5d719b33548f6e8
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:57:42 -0300

    Exercício Python 53: Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços. Exemplos de palíndromos: APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA.

--- Python/Exercícios/ex053.py
+++ Python/Exercícios/ex053.py
@@ -4,6 +4,7 @@ espaços. Exemplos de palíndromos:
 
 APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA.
 """
+
 f = input('Digite uma frase: ').strip().upper()
 
 # lista = f.split()
