ec07d37505e159a76bbb755ef78f9a8f01b74504
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:58:42 -0300

    Exercício Python 55: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.

--- Python/Exercícios/ex055.py
+++ Python/Exercícios/ex055.py
@@ -2,10 +2,10 @@
 Exercício Python 55: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e
 o menor peso lidos.
 """
+
 maior = 0
 menor = 0
-
-#lista = []
+# lista = []
 
 for i in range(1, 6):
     peso = float(input(f'Peso da {i}ª pessoa: '))
