00c147b7ade49cb073e8b3a635224cf7f4d440eb
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 02:05:24 -0300

    Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média

--- Python/Exercícios/ex007.py
+++ Python/Exercícios/ex007.py
@@ -1,6 +1,7 @@
 """
 Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média
 """
+
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota1+nota2)/2
