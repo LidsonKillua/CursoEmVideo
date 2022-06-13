aab44cddadf15ea9368dc392163f60201254cbf7
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:36:52 -0300

    Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.

--- Python/Exercícios/ex018.py
+++ Python/Exercícios/ex018.py
@@ -1,6 +1,7 @@
 """
 Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
 """
+
 from math import sin, cos, tan, radians
 n = radians(int(input('Digite o ângulo que você deseja: ')))
 print('O ângulo de {:.1f} tem o SENO de {:.2f}'.format(n, sin(n)))
