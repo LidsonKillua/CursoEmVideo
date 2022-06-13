8c6ab05f26e47747a742c75dfbb6573ec68c0507
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:43:51 -0300

    Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.

--- Python/Exercícios/ex028.py
+++ Python/Exercícios/ex028.py
@@ -3,6 +3,7 @@ Escreva um programa que faça o computador “pensar” em um número inteiro en
 descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou
 perdeu.
 """
+
 from random import randint
 from time import sleep
 
