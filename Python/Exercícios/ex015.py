d4d16028cd5bd838c8db4092de96912c8fd91bcb
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 02:07:56 -0300

    Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.

--- Python/Exercícios/ex015.py
+++ Python/Exercícios/ex015.py
@@ -2,6 +2,7 @@
 Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais
 ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
 """
+
 cdia = 60  # custo por dia
 ckm = 0.15  # custo por Km rodado
 qtddia = int(input('Quantos dias alugados? '))
