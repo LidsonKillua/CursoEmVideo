60f712abe5ad14d3dec1e49a364322a1760eab03
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:59:03 -0300

    Exercício Python 56: Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.

--- Python/Exercícios/ex056.py
+++ Python/Exercícios/ex056.py
@@ -2,6 +2,7 @@
 Exercício Python 56: Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa,
 mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
 """
+
 soma = 0
 cont = 0
 maior = 0
