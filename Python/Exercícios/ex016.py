7fce55e1cc1c50e476221593a26343634bfd5825
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 02:08:33 -0300

    Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.

--- Python/Exercícios/ex016.py
+++ Python/Exercícios/ex016.py
@@ -1,5 +1,6 @@
 """
 Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 """
+
 n = float(input('Digite um número: '))
 print('{:.2f} inteiro = {}'.format(n, int(n)))
