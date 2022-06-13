abbb67f8ee6ce804e021d612bd6680e751b66218
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 02:07:02 -0300

    Faça um algorítmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.

--- Python/Exercícios/ex012.py
+++ Python/Exercícios/ex012.py
@@ -1,6 +1,7 @@
 """
 Faça um algorítmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 """
+
 p = float(input('Qual o preço do produto? '))
 np = p-(p*0.05)
 print(' Promoção: \n De: R${:.2f} Por: R${:.2f}'.format(p, np))
