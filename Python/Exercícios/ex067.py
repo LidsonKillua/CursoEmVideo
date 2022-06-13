2d89b88343ea959310e6dcdddcb9a3780095ae79
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 10:11:30 -0300

    Exercício Python 67: Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.

--- Python/Exercícios/ex067.py
+++ Python/Exercícios/ex067.py
@@ -2,7 +2,6 @@
 Exercício Python 67: Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado
 pelo usuário. O programa será interrompido quando o número solicitado for negativo.
 """
-
 while True:
     print('-' * 30)
     val = int(input('Quer ver a tabuada de qual valor? '))
