d874fb1c200a1ebe74160f11376a51c05302adb9
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:57:08 -0300

    Exercício Python 51: Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.

--- Python/Exercícios/ex051.py
+++ Python/Exercícios/ex051.py
@@ -2,6 +2,7 @@
 Exercício Python 51: Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final,
 mostre os 10 primeiros termos dessa progressão.
 """
+
 print('=' * 40)
 print(f'{"10 TERMOS DE UMA PA":^40}')
 print('=' * 40)
