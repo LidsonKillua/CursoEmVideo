516b8ec9c3190ff4dad0faa1b1f2d8fdfa91c921
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:49:02 -0300

    Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.

--- Python/Exercícios/ex036.py
+++ Python/Exercícios/ex036.py
@@ -1,16 +1,16 @@
 """
 Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa,
 o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então
- o empréstimo será negado.
+o empréstimo será negado.
 """
 
-#Var
+# Var
 vcasa = float(input('Valor da casa: R$'))
 salario = float(input('Salário do comprador: R$'))
 anosfin = int(input('Quantos anos de financiamento? '))
 mesesfin = anosfin*12
 
-#Inicio
+# Inicio
 prest = vcasa/mesesfin
 trintaporc = salario * 0.3
 
