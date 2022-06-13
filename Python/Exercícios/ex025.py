007f72db38072a6e397ae8c8e084827d7240462a
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:41:52 -0300

    Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.

--- Python/Exercícios/ex025.py
+++ Python/Exercícios/ex025.py
@@ -1,6 +1,7 @@
 """
 Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
 """
+
 nome = str(input('Qual é o seu nome completo? ')).strip().upper()
 # Primeira forma que eu fiz:
 # i = nome.find('SILVA')
