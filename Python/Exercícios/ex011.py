2b4986ff2786d82a30f5c7c536592c0f96955005
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 02:06:47 -0300

    Faça um programa que leia a largura e a altura de uma parede em metros, e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m²

--- Python/Exercícios/ex011.py
+++ Python/Exercícios/ex011.py
@@ -3,6 +3,7 @@ Faça um programa que leia a largura e a altura de uma parede em metros,
 e a quantidade de tinta necessária para pintá-la, sabendo que cada
 litro de tinta pinta uma área de 2m²
 """
+
 larg = float(input('Quantos metros de largura? '))
 alt = float(input('Quantos metros de altura? '))
 area = larg*alt
