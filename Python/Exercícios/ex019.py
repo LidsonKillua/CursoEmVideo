25bd7492192ed3359d68a60cd7de9e161dbf4632
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:37:18 -0300

    Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.

--- Python/Exercícios/ex019.py
+++ Python/Exercícios/ex019.py
@@ -2,6 +2,7 @@
 Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele,
 lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
 """
+
 import random
 al1 = input('Primeiro aluno: ')
 al2 = input('Segundo aluno: ')
