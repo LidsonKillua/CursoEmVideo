c406fa76a2bf1471af538e65ab297c4c19f2287c
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:56:35 -0300

    Exercício Python 49: Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.

--- Python/Exercícios/ex049.py
+++ Python/Exercícios/ex049.py
@@ -2,6 +2,7 @@
 Exercício Python 49: Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher,
 só que agora utilizando um laço for.
 """
+
 num = int(input('Digite um número para ver sua tabuada: '))
 for i in range(1, 11):
     print(f'{num} x {i:2d} = {num * i}')
