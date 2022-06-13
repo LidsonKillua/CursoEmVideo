a77508ba47d0e634fc05ceb6f50701e4c6ddbea4
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:46:37 -0300

    Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%

--- Python/Exercícios/ex034.py
+++ Python/Exercícios/ex034.py
@@ -2,6 +2,7 @@
 Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
 Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%
 """
+
 sal = float(input('Qual é o salário do funcionário? R$'))
 aumento = 0.10 if sal > 1250.00 else 0.15  # Porcentagem de aumento (0.10x = 10%)
 print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(sal, sal + sal * aumento))
