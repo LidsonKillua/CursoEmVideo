2bf28ec5ab39b077fe7e4e903f207096bb2c0fc9
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 10:10:57 -0300

    Exercício Python 66: Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).

--- Python/Exercícios/ex066.py
+++ Python/Exercícios/ex066.py
@@ -3,7 +3,6 @@ Exercício Python 66: Crie um programa que leia números inteiros pelo teclado. 
 digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma
 entre elas (desconsiderando o flag).
 """
-
 soma = qtd = 0
 while True:
     val = int(input('Digite um valor (999 para parar): '))
