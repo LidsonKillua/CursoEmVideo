5dff6d62ba4fdc8caebc210733596b8c6be3fc25
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 10:14:31 -0300

    Exercício Python 73: Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre: a) Os 5 primeiros times. b) Os últimos 4 colocados. c) Times em ordem alfabética. d) Em que posição está o time do São Paulo. Tabela Brasileirão 02/06/2022

--- Python/Exercícios/ex073.py
+++ Python/Exercícios/ex073.py
@@ -1,16 +1,11 @@
 """
-Tabela Brasileirão 02/06/2022
-
 Exercício Python 73: Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de
 Futebol, na ordem de colocação. Depois mostre:
-
 a) Os 5 primeiros times.
-
 b) Os últimos 4 colocados.
-
 c) Times em ordem alfabética.
-
-d) Em que posição está o time da Chapecoense.
+d) Em que posição está o time do São Paulo.
+Tabela Brasileirão 02/06/2022
 """
 
 
