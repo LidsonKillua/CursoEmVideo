0288b7b79250ee873322220a29700b8cef5f3ab0
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 09:52:33 -0300

    Exercício Python 040: Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida: – Média abaixo de 5.0: REPROVADO – Média entre 5.0 e 6.9: RECUPERAÇÃO – Média 7.0 ou superior: APROVADO

--- Python/Exercícios/ex040.py
+++ Python/Exercícios/ex040.py
@@ -7,6 +7,7 @@ Exercício Python 040: Crie um programa que leia duas notas de um aluno e calcul
 
 – Média 7.0 ou superior: APROVADO
 """
+
 n1 = float(input('Primeira nota: '))
 n2 = float(input('Segunda nota: '))
 
