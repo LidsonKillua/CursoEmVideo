fd0a79449eefdce8f24d74248cae8e9bb3b35709
LidsonKillua <lidsonvini2@gmail.com>
2022-06-13 02:06:00 -0300

    Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.

--- Python/Exercícios/ex009.py
+++ Python/Exercícios/ex009.py
@@ -1,6 +1,7 @@
 """
 Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
 """
+
 n = int(input('Digite um número: '))
 print()
 print(' {} x  1 = {}'.format(n, n*1))
